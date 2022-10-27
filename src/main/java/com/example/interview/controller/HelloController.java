76ab9fca47e0cbd5490f8ac8a3cbec506851d71a
Minh Nguyen <minhnguyen@Minhs-MacBook-Air.local>
2022-10-27 14:10:11 +0900

    First commit

---
+++ src/main/java/com/example/interview/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.interview.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(method = RequestMethod.GET, path = "/hello")
+    public String hello() {
+        return "good morning";
+    }
+}
